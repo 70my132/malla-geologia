8ad916828917d98c308a69dd54a6d02dd7ca67c2
70my132 <tomasq.alvarez@gmail.com>
2025-07-13 18:56:20 -0400

    Add files via upload

---
+++ malla.js
@@ -0,0 +1,302 @@
+const malla = [
+  {
+    "semestre": 1,
+    "ramos": [
+      {
+        "nombre": "Álgebra",
+        "desc": "",
+        "grupo": "mat"
+      },
+      {
+        "nombre": "Introducción al Cálculo",
+        "desc": "",
+        "grupo": "mat"
+      },
+      {
+        "nombre": "Taller de Aptitudes Lógicas y Matemáticas",
+        "desc": "",
+        "grupo": "mat"
+      },
+      {
+        "nombre": "Química General",
+        "desc": "",
+        "grupo": "cien"
+      },
+      {
+        "nombre": "Antropología",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  },
+  {
+    "semestre": 2,
+    "ramos": [
+      {
+        "nombre": "Cálculo Diferencial e Integral",
+        "desc": "",
+        "grupo": "mat"
+      },
+      {
+        "nombre": "Álgebra Lineal",
+        "desc": "",
+        "grupo": "mat"
+      },
+      {
+        "nombre": "Física",
+        "desc": "",
+        "grupo": "cien"
+      },
+      {
+        "nombre": "Ética",
+        "desc": "",
+        "grupo": "inte"
+      },
+      {
+        "nombre": "Introducción a la Geología",
+        "desc": "",
+        "grupo": "geo"
+      }
+    ]
+  },
+  {
+    "semestre": 3,
+    "ramos": [
+      {
+        "nombre": "Cartografía Geológica y Sistemas de Información Geográfica",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Taller de Sustentabilidad",
+        "desc": "",
+        "grupo": "inte"
+      },
+      {
+        "nombre": "Taller de Programación I",
+        "desc": "",
+        "grupo": "comp"
+      },
+      {
+        "nombre": "Cristalografía y Mineralogía",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Geomorfología",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Persona y Sociedad",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  },
+  {
+    "semestre": 4,
+    "ramos": [
+      {
+        "nombre": "Geoquímica",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Taller de Programación II",
+        "desc": "",
+        "grupo": "comp"
+      },
+      {
+        "nombre": "Modelación Matemática en Geociencias",
+        "desc": "",
+        "grupo": "mat"
+      },
+      {
+        "nombre": "Hidrogeología",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Electivo I: Formación Integral",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  },
+  {
+    "semestre": 5,
+    "ramos": [
+      {
+        "nombre": "Petrología Ígnea",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Geología Estructural",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Petrología Sedimentaria",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Taller de Tecnologías Digitales",
+        "desc": "",
+        "grupo": "comp"
+      },
+      {
+        "nombre": "Gestión del riesgo Geológico",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  },
+  {
+    "semestre": 6,
+    "ramos": [
+      {
+        "nombre": "Petrología Metamórfica",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Estratigrafía y Geología Histórica",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Ingeniería Geologógica y Geotecnia",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Análisis de Datos Geológicos",
+        "desc": "",
+        "grupo": "comp"
+      },
+      {
+        "nombre": "Gestión para la Empleabilidad",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  },
+  {
+    "semestre": 7,
+    "ramos": [
+      {
+        "nombre": "Exploración Geológica y Geología de Minas",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Termodinámica Aplicada a la Geología",
+        "desc": "",
+        "grupo": "cien"
+      },
+      {
+        "nombre": "Caracterización de Minerales",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Geofísica",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Gestión Personal y Habilidades para la Vida",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  },
+  {
+    "semestre": 8,
+    "ramos": [
+      {
+        "nombre": "Geología Andina y Geopatrimonio",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Paleontología",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Taller de Emprendimiento",
+        "desc": "",
+        "grupo": "inte"
+      },
+      {
+        "nombre": "Geología de Campo I",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Gestión en Equipos para el Alto Desempeño",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  },
+  {
+    "semestre": 9,
+    "ramos": [
+      {
+        "nombre": "Formulación y Evaluación de Proyectos",
+        "desc": "",
+        "grupo": "inte"
+      },
+      {
+        "nombre": "Geología de Campo II",
+        "desc": "",
+        "grupo": "geo"
+      },
+      {
+        "nombre": "Taller en Empresa I",
+        "desc": "",
+        "grupo": "inte"
+      },
+      {
+        "nombre": "Práctica Profesional",
+        "desc": "",
+        "grupo": "inte"
+      },
+      {
+        "nombre": "Electivo de Profundización I",
+        "desc": "",
+        "grupo": "prof"
+      }
+    ]
+  },
+  {
+    "semestre": 10,
+    "ramos": [
+      {
+        "nombre": "Vía de Titulación(*)",
+        "desc": "",
+        "grupo": "tit"
+      },
+      {
+        "nombre": "Práctica Industrial",
+        "desc": "",
+        "grupo": "inte"
+      },
+      {
+        "nombre": "Electivo de Profundización II",
+        "desc": "",
+        "grupo": "prof"
+      },
+      {
+        "nombre": "Gestión de Carrera y Desarrollo Profesional",
+        "desc": "",
+        "grupo": "inte"
+      }
+    ]
+  }
+];
